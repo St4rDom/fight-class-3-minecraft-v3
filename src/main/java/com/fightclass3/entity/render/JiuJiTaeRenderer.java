044f40db133b39f2d9ab3cd05186f101b871842b
St4rDom <akshobhya.kulkarni@gmail.com>
2026-06-24 23:30:06 -0500

    Update JiuJiTaeRenderer.java

--- src/main/java/com/fightclass3/entity/render/JiuJiTaeRenderer.java
+++ src/main/java/com/fightclass3/entity/render/JiuJiTaeRenderer.java
@@ -3,13 +3,10 @@ package com.fightclass3.entity.render;
 import com.fightclass3.FightClass3Mod;
 import com.fightclass3.entity.JiuJiTaeEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Axis;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelLayers;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
-import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
 import net.minecraft.resources.ResourceLocation;
 
 public class JiuJiTaeRenderer extends HumanoidMobRenderer<JiuJiTaeEntity, HumanoidModel<JiuJiTaeEntity>> {
@@ -19,10 +16,6 @@ public class JiuJiTaeRenderer extends HumanoidMobRenderer<JiuJiTaeEntity, Humano
 
     public JiuJiTaeRenderer(EntityRendererProvider.Context ctx) {
         super(ctx, new HumanoidModel<>(ctx.bakeLayer(ModelLayers.ZOMBIE)), 0.5f);
-        this.addLayer(new HumanoidArmorLayer<>(this,
-                new HumanoidModel<>(ctx.bakeLayer(ModelLayers.ZOMBIE_INNER_ARMOR)),
-                new HumanoidModel<>(ctx.bakeLayer(ModelLayers.ZOMBIE_OUTER_ARMOR)),
-                ctx.getModelSet()));
     }
 
     @Override
@@ -30,6 +23,6 @@ public class JiuJiTaeRenderer extends HumanoidMobRenderer<JiuJiTaeEntity, Humano
 
     @Override
     protected void scale(JiuJiTaeEntity entity, PoseStack pose, float partialTick) {
-        pose.scale(1.0f, 1.0f, 1.0f); // Standard player size
+        pose.scale(1.0f, 1.0f, 1.0f);
     }
 }
